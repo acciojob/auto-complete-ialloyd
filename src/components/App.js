7f9a03e23f94b2a7b7a094ee911926d923f0476b
ialloyd <iamalloydfds@gmail.com>
2023-10-19 09:58:47 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+
 const App = () => {
+
+  const [filtered, setItems] = useState(fruits);
+
+  function filterItems(event) {
+
+    const arr = fruits.filter((fruit) => fruit.includes(event.target.value))
+    setItems(arr)
+
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Search Item</h1>
+      <input onChange={(event) => filterItems(event)} />
+      <ul>
+        {filtered.map(fruit => <li>{fruit}</li>)}
+      </ul>
     </div>
   )
 }
